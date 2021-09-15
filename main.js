4949b620de3c67852597540e9a2fb804fcad711b
SAMPIPROUSER <90777274+SAMPIPROUSER@users.noreply.github.com>
2021-09-15 19:19:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+canvas= document.getElementById('myCanvas');
+ctx = canvas.getContext("2d")
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;   
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("key_down", my_keydown)
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		( ‘Alphabet’,‘Number’, ‘Arrow’, ‘Special’, and ‘Other’ keys)
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	Image="Alpkey.png"
+
+}
+function numberkey()
+{
+	Image="numkey.png"
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
